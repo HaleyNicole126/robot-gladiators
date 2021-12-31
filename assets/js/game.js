8430c5d75a068bb993944458720146eba94e0afe
haybales <haleynicole126@gmail.com>
2021-12-30 17:15:03 -0700

    battle progress

--- assets/js/game.js
+++ assets/js/game.js
@@ -14,10 +14,9 @@ var enemyHealth = 50;
 var enemyAttack = 12; 
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
-
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    // repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0){
+        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
     // if a player choses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -69,8 +68,12 @@ var fight = function(enemyName) {
         window.alert("You need to choose a valid option. Try again!");
     }
     };
+
+    }
     
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
-}
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
+  }
